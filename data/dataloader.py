ddfd30c03fc9ebe7c6de2a55008899b208f5b59b
CG_Lambda <gchakradhar72@gmail.com>
2024-01-31 00:01:48 -0500

    Fixes bugs

--- data/dataloader.py
+++ data/dataloader.py
@@ -4,6 +4,23 @@ import numpy as np
 from data.dataset import IDPBERTDataset
 
 
+def select_label(train_y, val_y, test_y, label):
+    if label == 'rog':
+        train_y = train_y[:, 0]
+        val_y = val_y[:, 0]
+        test_y = test_y[:, 0]
+    elif label == 'cv':
+        train_y = train_y[:, 1]
+        val_y = val_y[:, 1]
+        test_y = test_y[:, 1]
+    elif label == 'tau':
+        train_y = train_y[:, 2]
+        val_y = val_y[:, 2]
+        test_y = test_y[:, 2]
+
+    return train_y, val_y, test_y
+
+
 def load_data(config):
     with np.load('./data/train.npz') as train,\
          np.load('./data/val.npz') as val,\
@@ -12,15 +29,17 @@ def load_data(config):
         val_X, val_y = val['X'], val['y']
         test_X, test_y = test['X'], test['y']
 
+    train_y, val_y, test_y = select_label(train_y, val_y, test_y, config['label'])
+
     tokenizer = BertTokenizerFast.from_pretrained('Rostlab/prot_bert_bfd', do_lower_case=False)
 
-    encoded_tr = tokenizer(train_X, padding='max_length', max_length=400, return_attention_mask=True)
+    encoded_tr = tokenizer(list(train_X), padding='max_length', max_length=400, return_attention_mask=True)
     train_encodings, train_attention_masks = encoded_tr['input_ids'], encoded_tr['attention_mask']
 
-    encoded_vl = tokenizer(val_X, padding='max_length', max_length=400, return_attention_mask=True)
+    encoded_vl = tokenizer(list(val_X), padding='max_length', max_length=400, return_attention_mask=True)
     val_encodings, val_attention_masks = encoded_vl['input_ids'], encoded_vl['attention_mask']
 
-    encoded_te = tokenizer(test_X, padding='max_length', max_length=400, return_attention_mask=True)
+    encoded_te = tokenizer(list(test_X), padding='max_length', max_length=400, return_attention_mask=True)
     test_encodings, test_attention_masks = encoded_te['input_ids'], encoded_te['attention_mask']
 
     train_dataset = IDPBERTDataset(input_ids=train_encodings, attention_masks=train_attention_masks, labels=train_y)
